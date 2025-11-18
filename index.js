d3b6a9cd1b02cd9afdb2fa2b1b07f53cd3c4913b
b0tinho <janek.lochman@gmail.com>
2025-11-18 09:11:17 +0100

    Com4_1.1

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const app = express();
 
 // define endpoint for exercise 1 here
 app.get('/math/circle/:r', (req, res) => {
-//TODO1  
-  res.json(result);
+   const r = parseFloat(req.params.r);
+  if (isNaN(r) || r <= 0) {
+    return res.status(400).json({ error: 'Promień musi być liczbą większą od 0.' });
+  }
+
+  const area = Math.PI * r * r;
+  const circumference = 2 * Math.PI * r;
+
+  const result = {
+    area: area.toFixed(2),
+    circumference: circumference.toFixed(2)
+  }; 
+  res.json(result)
 });
 
 //TODO2
